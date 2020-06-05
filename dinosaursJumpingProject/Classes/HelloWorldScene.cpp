dca81a05425d3660704d6f49572942a7d4e036c5
chicken-developer <quynh.fullstackdev@gmail.com>
2020-06-06 01:12:35 +0700

    [Quynh] Update code for animation

--- dinosaursJumpingProject/Classes/HelloWorldScene.cpp
+++ dinosaursJumpingProject/Classes/HelloWorldScene.cpp
@@ -22,49 +22,41 @@ bool HelloWorld::init(){
 
     SpriteFrameCache::getInstance()->addSpriteFramesWithFile("Textures.plist");
 
-
+    //Auto Layout
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
-
     float deviceHeight = MAX(glview->getFrameSize().height, glview->getFrameSize().width);
     float deviceWidth = MIN(glview->getFrameSize().height, glview->getFrameSize().width);
     deviceHeight = deviceHeight/ deviceWidth * 600;
     deviceWidth = 600;
 
-
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr){
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else{
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
-
-        this->addChild(label, 1);
-    }
-
+    //Game title
     auto spGameTitle = Sprite::createWithSpriteFrameName("title.png");
     if(spGameTitle == nullptr){
         problemLoading("Sprite Game Title");
     }
     else{
         spGameTitle->setAnchorPoint(Vec2::ANCHOR_MIDDLE_TOP);
-        spGameTitle->setPosition(origin.x + visibleSize.width /2, origin.y + visibleSize.height - TITLE_INITIAL_Y);
+        spGameTitle->setPosition(origin.x + visibleSize.width /2,
+                                 origin.y + visibleSize.height - TITLE_INITIAL_Y);
         this->addChild(spGameTitle,2);
     }
 
+    //Game over sprites
     auto spGameOver = Sprite::createWithSpriteFrameName("gameover.png");
     if(spGameOver == nullptr){
         problemLoading("Sprite Game Over");
     }
     else{
         spGameOver->setAnchorPoint(Vec2::ANCHOR_MIDDLE_TOP);
-        spGameOver->setPosition(deviceWidth/ 2, deviceHeight - TITLE_HIDDEN_Y);
+        spGameOver->setPosition(origin.x + visibleSize.width/ 2,
+                                origin.y + visibleSize.height - TITLE_HIDDEN_Y);
         this->addChild(spGameOver);
     }
 
+    //Road 1 Sprite
     auto spRoad_1 = Sprite::createWithSpriteFrameName("road_1.png");
     if(spRoad_1 == nullptr){
         problemLoading("Sprite Road 1");
@@ -75,16 +67,19 @@ bool HelloWorld::init(){
         this->addChild(spRoad_1);
     }
 
-     auto spRoad_2 = Sprite::createWithSpriteFrameName("road_2.png");
+    //Road 2 Sprite
+    auto spRoad_2 = Sprite::createWithSpriteFrameName("road_2.png");
     if(spRoad_2 == nullptr){
         problemLoading("Sprite Road 2");
     }
     else{
         spRoad_2->setAnchorPoint(Vec2::ANCHOR_MIDDLE_LEFT);
-        spRoad_2->setPosition(spRoad_1->getPositionX() + spRoad_1->getContentSize().width - 5, GROUND_HEIGHT);
+        spRoad_2->setPosition(spRoad_1->getPositionX() + spRoad_1->getContentSize().width - 5,
+                              GROUND_HEIGHT);
         this->addChild(spRoad_2);
     }
-
+    
+    //Dinosaurs
     auto spDino = Sprite::createWithSpriteFrameName("dino_1.png");
     if(spDino == nullptr){
         problemLoading("Sprite Dino");
@@ -95,10 +90,59 @@ bool HelloWorld::init(){
         this->addChild(spDino);
     }
 
+    
 
+    //Background
+    auto spBackground = Sprite::createWithSpriteFrameName("background.png");
+    if(spBackground == nullptr){
+        problemLoading("Sprite background");
+    }
+    else{
+        spBackground->setPosition(origin.x + visibleSize.width/ 2,
+                                  origin.y + visibleSize.height/ 2);
+        this->addChild(spBackground);
+    }
 
+    //Dino Run Animation
+    auto dinoFrame = getAnimation("dino_%01d.png",2);
+    auto dinoSprite = Sprite::createWithSpriteFrame(dinoFrame.front());
+    if(dinoSprite == nullptr){
+        problemLoading("Sprites Dino Animation");
+    }
+    else{
+        spBackground->addChild(dinoSprite);
+        dinoSprite->setPosition( Vec2( origin.x + visibleSize.width/ 2,
+                                GROUND_HEIGHT - 140));
+    }
+
+    //Hand Tap Animation 
+    auto handFrame = getAnimation("hand_%01d.png",2);
+    auto handSprite = Sprite::createWithSpriteFrame(handFrame.front());
+    if(handSprite == nullptr){
+        problemLoading("Sprites Hand Animation");
+    }
+    else{
+        spBackground->addChild(handSprite);
+        dinoSprite->setPosition( Vec2( origin.x + visibleSize.width/ 2,
+                                GROUND_HEIGHT - 140));
+    }
     return true;
 }
+
+/*
+* Animation
+*/
+Vector HelloWorld::getAnimation(const char* format, int count){
+    auto spriteCache = SpriteFrameCache::getInstance();
+    Vector animFrames;
+    char str[100];
+    for(int i = 1; i <= count; i++){
+        sprintf(str, format,i);
+        animFrames.pushBack(spriteCache->getSpriteFrameByName(str));
+    }
+    return animFrames;
+}
+
 
 void HelloWorld::menuCloseCallback(Ref* pSender){
     Director::getInstance()->end();
--- dinosaursJumpingProject/Classes/HelloWorldScene.h
+++ dinosaursJumpingProject/Classes/HelloWorldScene.h
@@ -9,12 +9,14 @@ public:
     static cocos2d::Scene* createScene();
 
     virtual bool init();
-    
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+private:
+    cocos2d::Vector getAnimation(const char* format, int count);
+
 };
 
 #endif // __HELLOWORLD_SCENE_H__
